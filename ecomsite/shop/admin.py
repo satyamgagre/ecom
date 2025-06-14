9fb7cf6f497dfaf3e939765a8329832fc8b89fc1
Satyam Gagare <satyamgagre4@gmail.com>
2025-06-14 22:29:12 +0530

    E COMMERCE

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Products
+from .models import Order
 # Register your models here.
 
+admin.site.site_header = "E-Commerce Site"
+admin.site.site_title = "E-Commerce Site"
+admin.site.index_title ="Manage E-Commerce Site"
+
 admin.site.register(Products)
+admin.site.register(Order)
--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -13,3 +13,17 @@ class Products(models.Model):
     description = models.TextField()
     image = models.CharField(max_length=300)
 
+
+class Order(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    items = models.CharField(max_length=1000)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    address = models.CharField(max_length=1000)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=6)
+    total = models.CharField(max_length=100)
